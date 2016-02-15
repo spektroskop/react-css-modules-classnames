59dc47324d28108953b2e9173c1cae5ef9571061
Alexander Stensrud <alex@stensrud.nu>
2016-02-15 06:05:09 +0100

    Add styles prop

--- src/classnames.js
+++ src/classnames.js
@@ -1,17 +1,20 @@
 import React from 'react'
-import { compose, curry, has, is, lensProp, over, split, when } from 'ramda'
-import { isClassComponent, setDisplayName } from 'recompose'
+import { always, compose, curry, has, is, lensProp, merge, over, split, when } from 'ramda'
 import cn from 'classnames/bind'
+import { isClassComponent } from 'react-util'
 
 const wrap = curry((styles, element) => {
 	if (!React.isValidElement(element)) { return element }
 
 	const children = React.Children.map(element.props.children, wrap(styles))
-	const props = when(has('className'), over(lensProp('className'),
-		compose(styles, when(is(String), split(' ')))
-	))
+	const transform = compose(
+		when(always(is(Function, element.type)), merge({ styles })),
+		when(has('className'), over(lensProp('className'),
+			compose(styles, when(is(String), split(' ')))
+		))
+	)
 
-	return React.cloneElement(element, props(element.props), children)
+	return React.cloneElement(element, transform(element.props), children)
 })
 
 const classnames = (styles) => (component) => {
@@ -22,11 +25,12 @@ const classnames = (styles) => (component) => {
 			}
 		}
 	} else {
-		const wrapped = setDisplayName(component.name, (props) =>
-			wrap(cn.bind(styles), component(props))
-		)
+		const wrapped = (props) => wrap(cn.bind(styles), component(props))
+
 		wrapped.defaultProps = component.defaultProps
 		wrapped.propTypes = component.propTypes
+		wrapped.displayName = component.displayName || component.name
+
 		return wrapped
 	}
 }
